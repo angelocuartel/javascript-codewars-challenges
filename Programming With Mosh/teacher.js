f9ed04fd4af8f3a4e0be3f27780cecd9251494e6
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-09-02 17:07:34 +0800

    deletetrashfiles

--- Programming With Mosh/teacher.js
+++
@@ -1,9 +0,0 @@
- import { Person } from "./person.js";
-
-export class Teacher extends Person{
-    constructor(name , degree){
-        super(name);
-    }
-    
-}
-
--- notSecure.js
+++
@@ -1,8 +0,0 @@
-function alphaNumeric(string){
- 
-}
-
-let arr = [3,23,1,5];
-for( n in arr){
-    console.log(arr[n]);
-}
